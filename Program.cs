a574758ca1c503dd8403ac091328aae96be3b93d
Henry <hnr.leto11@gmail.com>
2021-03-27 12:06:04 -0400

    update puzzles

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace CSharpPuzzles
             // sol.SumZero(5);
             // sol.Shuffle("aiohn", new int[]{3, 1, 4, 2, 0});
             // sol.KidsWithCandies(new int[]{2,3,5,1,3}, 3);
-            sol.LargeGroupPositions("abbxxxxzzy");
+            // sol.LargeGroupPositions("abbxxxxzzy");
+            // int[] p = { 1, 2, 3 };
+            // sol.MaximumWealth(new int[][] { p, p });
+            // sol.StartsWith("samurai", "ai");
+            // sol.ToCamelCase("the-stealth-warrior");
+            sol.CreatePhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+            sol.FakeBin("50001223567");
         }
     }
 }
--- Solution.cs
+++ Solution.cs
@@ -53,17 +53,75 @@ namespace CSharpPuzzles
             return ans;
         }
 
-        public void LargeGroupPositions(string s)
-        {
-            List<int> ans = new List<int>();
+        // public void LargeGroupPositions(string s)
+        // {
+        //     // List<int> ans = new List<int>();
 
-            for (int i = 0; i < s.Length; i++)
+        //     // for (int i = 0; i < s.Length; i++)
+        //     // {
+        //     //     if (i < s.Length && s[i].Equals(s[i + 1]))
+        //     //     {
+        //     //         ans.Add(i);
+        //     //     }
+        //     // }
+        // }
+
+        public bool StartsWith(string str, string ending)
+        {
+            if (str.EndsWith(ending))
             {
-                if (i < s.Length && s[i].Equals(s[i + 1]))
+                return true;
+            }
+            return false;
+        }
+
+        public void ToCamelCase(string str)
+        {
+            List<char> list = new List<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '-')
                 {
-                    ans.Add(i);
+                    list.Add(char.ToUpper(str[i + 1]));
+                }
+                else if ((str[i]) != '-' && !char.IsUpper(str[0]))
+                {
+                    if (char.IsUpper(str[i - 1]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        list.Add(str[i]);
+                    }
                 }
             }
+        }
+
+        public string CreatePhoneNumber(int[] numbers)
+        {
+            var AreaCode = numbers.Skip(0).Take(3).ToString();
+            var FirstThree = numbers.Skip(3).Take(6).ToString();
+            var LastFour = numbers.Skip(6).Take(10).ToString();
+            var q =  $"({AreaCode}) {FirstThree}-{LastFour}";
+            return q;
+        }
+
+        public string FakeBin(string x)
+        {
+            var list = new List<int>();
+            foreach (var i in x)
+            {
+                if (i < 5)
+                {
+                    list.Add(0);
+                }
+                else if (i >= 5)
+                {
+                    list.Add(5);
+                }
+            }
+            return list.ToString();
         }
     }
 }
